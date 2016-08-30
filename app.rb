24b43d777762fb78e0d19d7c97740125da2c9e89
Abigail <abigail.mcphillips@gmail.com>
2016-08-30 15:24:24 +0100

    adds dotted red border

--- app.rb
+++ app.rb
@@ -9,5 +9,11 @@ get '/about' do
 end
 
 get '/secret' do
-    'this is a secret webpage aashflaskjdfhaljdsfj;aoejf;oawefojaewijf;laksdjfl;kasdjf;lkadflkjsd'
+    'this is a secret webpage'
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
+  </div>"
 end
