a82e413c053e009838f5ca4e3d1206357d163c54
Tam Borine <tamborine@Tams-MacBook-Air.local>
2016-08-30 13:18:22 +0100

    stuffs

--- app.rb
+++ app.rb
@@ -5,3 +5,12 @@ puts "this is a process you can kill me shamelessly with kill #{Process.pid}"
 get '/' do
   "Hello!"
 end
+
+get '/food' do
+  'yummy stuff not frfom toms take away app'
+end
+
+
+get '/secret' do
+  "lalalala"
+end
